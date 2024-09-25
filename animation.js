ec374b12e771ca1605971899629626a447cf672a
iqbaladiatma <gazamuhammadadiatmav@gmail.com>
2024-09-25 09:37:23 +0700

    web

---
+++ animation.js
@@ -0,0 +1,25 @@
+// Mendapatkan elemen dengan ID "animateBox"
+const box = document.getElementById('animateBox');
+let position = 0; 
+let direction = 1; 
+
+// Fungsi untuk menjalankan animasi
+function animate() {
+    // Jika elemen mencapai batas kanan layar
+    if (position >= window.innerWidth - 50) {
+        direction = -1; 
+    } else if (position <= 0) {
+        direction = 1; 
+    }
+
+    // Mengubah posisi elemen berdasarkan arah
+    position += 5 * direction; 
+    box.style.left = position + 'px'; 
+
+    // Meminta frame berikutnya untuk animasi agar halus
+    requestAnimationFrame(animate);
+}
+
+// Memulai animasi saat halaman di-load
+
+animate(:)
